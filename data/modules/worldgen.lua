b4c3056dcaa0fffc763bf22297fe6fb70ef1efeb
Nanskip <nanskip.oof@gmail.com>
2024-04-27 16:18:42 +0300

    clouds + rivers in planet generator

--- data/modules/worldgen.lua
+++ data/modules/worldgen.lua
@@ -2,7 +2,7 @@ worldgen = {}
 
 worldgen.generatePlanet = function(config)
     defaultConfig = {
-        scale = 16,
+        scale = 64,
         colors = {
             deepWater = Color(26, 51, 92),
             water = Color(34, 63, 110),
@@ -21,14 +21,27 @@ worldgen.generatePlanet = function(config)
 
     planet = MutableShape(Items.nanskip.v)
     planet.layer2 = MutableShape(Items.nanskip.v)
-    planet.layer2.Scale = planet.Scale + (planet.Scale*0.02)
+    planet.layer2.Scale = planet.Scale + (planet.Scale*0.02*(16/scale))
     planet.layer2:SetParent(planet)
     planet.layer3 = MutableShape(Items.nanskip.v)
-    planet.layer3.Scale = planet.Scale + (planet.Scale*0.04)
+    planet.layer3.Scale = planet.Scale + (planet.Scale*0.04*(16/scale))
     planet.layer3:SetParent(planet)
+    planet.cloudLayer1 = MutableShape(Items.nanskip.v)
+    planet.cloudLayer1:SetParent(planet)
+    planet.cloudLayer1.Scale = planet.Scale + (planet.Scale*0.08*(16/scale))
+    planet.cloudLayer2 = MutableShape(Items.nanskip.v)
+    planet.cloudLayer2:SetParent(planet)
+    planet.cloudLayer2.Scale = planet.Scale + (planet.Scale*0.12*(16/scale))
     
     local getBl = function(x, y, z)
-        local num = perlin.get(x*zoom, y*zoom, z*zoom) + perlin.get(x*zoom/2+5015, y*zoom/2+50215, z*zoom/2+50346) - perlin.get(x*zoom/4+50, y*zoom/4+500, z*zoom/4+5000)
+        local num = perlin.get(x*zoom, y*zoom, z*zoom) + perlin.get(x*zoom/2+515, y*zoom/2+515, z*zoom/2+546) - perlin.get(x*zoom/4+572, y*zoom/4+60, z*zoom/4+520)
+        local riverNum = perlin.get(x*zoom/5+260, y*zoom/5+3671, z*zoom/5+1610)
+        if riverNum > -0.2/2 and riverNum < -0.1/2 then
+            if num >= -0.2 and num < 0.4 then num = -0.3 end
+        elseif (riverNum <= -0.2/2 and riverNum > -0.25/2) or (riverNum >= -0.1/2 and riverNum < -0.05/2) then
+            if num >= -0.05 and num < 0.4  then num = -0.1 end
+        end
+
         local col = Color(255, 255, 255)
 
         if num < -0.5 then
@@ -64,38 +77,63 @@ worldgen.generatePlanet = function(config)
             if math.random(0, 5) ~= 0 then planet.layer3:AddBlock(block2) end
         end
         planet:AddBlock(block2)
+
+        local cloudNum = perlin.get(x*zoom, y*zoom, z*zoom) + perlin.get(x*zoom/2+50215, y*zoom/2+515, z*zoom/2+546) - perlin.get(x*zoom/4+5260, y*zoom/4+56200, z*zoom/4+5067100)
+        cloudNum = math.floor((cloudNum + 1)*4)/4/16
+        local cloudBlock = Block(Color(255, 255, 255, cloudNum), Number3(x, y, z))
+        planet.cloudLayer1:AddBlock(cloudBlock)
+
+        local cloudNum = perlin.get(x*zoom, y*zoom, z*zoom) + perlin.get(x*zoom/2+5115, y*zoom/2+5615, z*zoom/2+5676) - perlin.get(x*zoom/4+1560, y*zoom/4+5634, z*zoom/4+502600)
+        cloudNum = math.floor((cloudNum + 1)*4)/4/16
+        local cloudBlock = Block(Color(255, 255, 255, cloudNum), Number3(x, y, z))
+        planet.cloudLayer2:AddBlock(cloudBlock)
     end
 
-    for x=scale*-0.5, scale do
+    Timer(0.1, false, function()
+        for x=scale*-0.5, scale do
+            for y=scale*-0.5, scale do
+                genBlock(x, y, scale*-0.5)
+            end
+        end
+    end)
+    Timer(0.2, false, function()
+        for x=scale*-0.5, scale do
+            for z=scale*-0.5, scale do
+                genBlock(x, scale*-0.5, z)
+            end
+        end
+    end)
+    Timer(0.3, false, function()
         for y=scale*-0.5, scale do
-            genBlock(x, y, scale*-0.5)
+            for z=scale*-0.5, scale do
+                genBlock(scale*-0.5, y, z)
+            end
         end
-    end
-    for x=scale*-0.5, scale do
-        for z=scale*-0.5, scale do
-            genBlock(x, scale*-0.5, z)
+    end)
+    Timer(0.4, false, function()
+        for x=scale*-0.5, scale do
+            for y=scale*-0.5, scale do
+                genBlock(x, y, scale)
+            end
         end
-    end
-    for y=scale*-0.5, scale do
-        for z=scale*-0.5, scale do
-            genBlock(scale*-0.5, y, z)
+    end)
+    Timer(0.5, false, function()
+        for x=scale*-0.5, scale do
+            for z=scale*-0.5, scale do
+                genBlock(x, scale, z)
+            end
         end
-    end
-    for x=scale*-0.5, scale do
+    end)
+    Timer(0.6, false, function()
         for y=scale*-0.5, scale do
-            genBlock(x, y, scale)
-        end
-    end
-    for x=scale*-0.5, scale do
-        for z=scale*-0.5, scale do
-            genBlock(x, scale, z)
-        end
-    end
-    for y=scale*-0.5, scale do
-        for z=scale*-0.5, scale do
-            genBlock(scale, y, z)
-        end
-    end
+            for z=scale*-0.5, scale do
+                genBlock(scale, y, z)
+            end
+        end    
+    end)
+    Timer(0.7, false, function()
+        planet.Pivot = Number3(planet.Width/planet.Scale.X, planet.Height/planet.Scale.Y, planet.Depth/planet.Scale.Z)/5
+    end)
 
     planet:SetParent(World)
 end
