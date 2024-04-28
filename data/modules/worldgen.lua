99555e9f8b6f2003c67eff716c43250780e88c06
Nanskip <nanskip.oof@gmail.com>
2024-04-28 10:30:56 +0300

    added sector generator using diamond-square

--- data/modules/worldgen.lua
+++ data/modules/worldgen.lua
@@ -138,4 +138,168 @@ worldgen.generatePlanet = function(config)
     end)
 end
 
+worldgen.generateSector = function(config)
+    local defaultConfig = {
+        colors = {
+            deepWater = Color(26, 51, 92),
+            water = Color(34, 63, 110),
+            beach = Color(140, 123, 65),
+            grass = Color(56, 120, 49),
+            forest = Color(59, 94, 45),
+            hills = Color(71, 74, 73),
+            mountains = Color(59, 64, 63)
+        },
+        scale = 129,
+        roughness = 30,
+        mapSize = 129,
+    }
+
+    local mergedConfig = {}
+    for k, v in pairs(defaultConfig) do
+        mergedConfig[k] = config[k] or v
+    end
+
+    local colors = mergedConfig.colors
+    local mapSize = mergedConfig.mapSize
+    local roughness = mergedConfig.roughness
+    local scale = mergedConfig.scale
+
+    map = {}
+    for i = 0, mapSize do
+        map[i] = {}
+        for j = 0, mapSize do
+            map[i][j] = 0
+        end
+    end
+
+    local function randomRange(min, max)
+        return math.random() * (max - min) + min
+    end
+
+    local function diamondSquare(map, size, roughness)
+        local halfSize = size / 2
+
+        if halfSize < 1 then
+            return
+            final()
+        end
+
+        for y = halfSize, mapSize - 1, size do
+            for x = halfSize, mapSize - 1, size do
+                
+                local average = (map[y - halfSize][x - halfSize] +
+                                 map[y - halfSize][x + halfSize] +
+                                 map[y + halfSize][x - halfSize] +
+                                 map[y + halfSize][x + halfSize]) / 4
+                map[y][x] = average + randomRange(-roughness, roughness)
+            end
+        end
+
+        for y = 0, mapSize - 1, halfSize do
+            for x = (y + halfSize) % size, mapSize - 1, size do
+                local sum = 0
+                local count = 0
+                if y - halfSize >= 0 then
+                    sum = sum + map[y - halfSize][x]
+                    count = count + 1
+                end
+                if y + halfSize < mapSize then
+                    sum = sum + map[y + halfSize][x]
+                    count = count + 1
+                end
+                if x - halfSize >= 0 then
+                    sum = sum + map[y][x - halfSize]
+                    count = count + 1
+                end
+                if x + halfSize < mapSize then
+                    sum = sum + map[y][x + halfSize]
+                    count = count + 1
+                end
+                map[y][x] = sum / count + randomRange(-roughness, roughness)
+            end
+        end
+
+        Timer(0.1, false, function()
+            diamondSquare(map, halfSize, roughness*0.75)
+        end)
+    end
+
+    map[1][1] = randomRange(-scale, scale)
+    map[1][mapSize] = randomRange(-scale, scale)
+    map[mapSize][1] = randomRange(-scale, scale)
+    map[mapSize][mapSize] = randomRange(-scale, scale)
+
+    diamondSquare(map, mapSize - 1, roughness)
+
+    final = function()
+
+        Timer(0.1, false, function()
+            for abob=1, 4 do
+                for x=0, mapSize do
+                    for y=0, mapSize do
+
+                        local neighbours = {
+                            {-1, 0},
+                            {0, -1},
+                            {1, 0},
+                            {0, 1},
+                            {-1, -1},
+                            {-1, 1},
+                            {1, -1},
+                            {1, 1}
+                        }
+                        local count = 0
+                        local avg = 0
+                        for i=1, #neighbours do
+                            if map[x+neighbours[i][1]][y+neighbours[i][2]] ~= nil then
+                                count = count + 1
+                                avg = avg + map[x+neighbours[i][1]][y+neighbours[i][2]]
+                            end
+                        end
+                        if count == 0 then count = 1 end
+                        avg = avg / count
+
+                        map[x][y] = (map[x][y] * 0.7 + avg * 0.3)
+                    end
+                end
+            end
+        end)
+
+
+        Timer(0.2, false, function()
+            
+            local sector = {}
+            for y = 1, mapSize do
+                sector[y] = {}
+                Timer(0.02*y, false, function()
+                    for x = 1, mapSize do
+                        local value = map[y][x]
+
+                        if value < -30 then
+                            sector[y][x] = colors.deepWater
+                        elseif value < -10 then
+                            sector[y][x] = colors.water
+                        elseif value < 5 then
+                            sector[y][x] = colors.beach
+                        elseif value < 20 then
+                            sector[y][x] = colors.grass
+                        elseif value < 40 then
+                            sector[y][x] = colors.forest
+                        elseif value < 60 then
+                            sector[y][x] = colors.hills
+                        else
+                            sector[y][x] = colors.mountains
+                        end
+
+                        local col = sector[y][x] or Color.Black
+                        local block = Block(col, Number3(x, y, 0))
+                        a:AddBlock(block)
+                    end
+                end)
+            end
+        end)
+    end
+    return sector
+end
+
 return worldgen
