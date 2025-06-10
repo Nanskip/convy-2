c0a8bd973f8640864c5cffd1f475e72bbcce4c50
Nazar Polyvoda <nanskip.oof@gmail.com>
2025-06-10 20:59:23 +0200

    added mathlib.lcg(seed) for pseudo random number generation

--- source/modules/mathlib.lua
+++ source/modules/mathlib.lua
@@ -3,3 +3,11 @@ local mathlib = {}
 function mathlib.lerp(a, b, t)
     return a + (b - a) * t
 end
+
+function mathlib.lcg(seed)
+    local current_seed = seed
+    return function()
+        current_seed = (1103515245 * current_seed + 12345) % 2147483648
+        return current_seed / 2147483648
+    end
+end
