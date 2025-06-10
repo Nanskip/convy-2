756f4f8d44c44f6533c0fd18540d142b858e52e2
Nazar Polyvoda <nanskip.oof@gmail.com>
2025-06-10 21:00:53 +0200

    added temprorary debug window on start

--- source/main.lua
+++ source/main.lua
@@ -31,6 +31,62 @@ Other = {
 _ON_START = function()
     loading_screen:intro()
     _UI:init()
+
+    -- temporary test
+    debugwindow = advanced_ui.createWindow({
+        title = "Debug",
+        width = 300,
+        height = 200,
+        pos = {Screen.Width - 350, Screen.Height - 250},
+        topbar_buttons = {
+            {
+                text = "X",
+                func = "close",
+                size = 14,
+                color = Color(237, 66, 24),
+                textcolor = Color(255, 255, 255)
+            }
+        }
+    })
+    debugwindow.createCube = debugwindow:createFrame({
+            pos = {50, 25},
+            size = {200, 50},
+            color = Color(255, 255, 255),
+        })
+    debugwindow.createCubeText = debugwindow:createText({
+            pos = {0, 0},
+            color = Color(0, 0, 0),
+            fontsize = 30,
+            text = "Generate Cube",
+        })
+    debugwindow.createCubeText.config.pos = {
+        (debugwindow.createCube.config.pos[1] + debugwindow.createCube.Width/2)
+        - (debugwindow.createCubeText.Width/2),
+
+        (debugwindow.createCube.config.pos[2] + debugwindow.createCube.Height/2)
+        - (debugwindow.createCubeText.Height/2)
+    }
+    debugwindow.createCubeText:update()
+
+    debugwindow.createCube.onPress = function(self)
+        self.config.color = Color(200, 200, 200)
+        self:update()
+        if _CUBE == nil then
+            seed = os.time()
+            _CUBE = worldgen.perlinCube({seed = seed})
+            _CUBE:SetParent(World)
+            debug.log("Cube generated.")
+        else
+            _CUBE:Destroy()
+            _CUBE = nil
+
+            seed = seed + 1
+            _CUBE = worldgen.perlinCube({seed = seed})
+            _CUBE:SetParent(World)
+            debug.log("Cube regenerated.")
+        end
+    end
+    -- temporary test
 end
 
 _ON_START_CLIENT = function()
